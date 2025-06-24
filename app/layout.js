1e0c60a549a63d7b79e6df51ff266580b46de038
Ismail PE <mailismailpe@gmail.com>
2025-06-24 11:16:42 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -19,8 +19,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <head><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9138119079477333"
-     crossorigin="anonymous"></script></head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
